144fde76bd99c91d67a42a4d1e0edb9a8d5f14ab
katebartnik <oferta.bartnik@gmail.com>
2019-11-09 11:55:17 +0100

    Snake game

---
+++ kasia/Snake.py
@@ -0,0 +1,132 @@
+import turtle
+import time
+import random
+
+delay = 0.01
+
+#Set up the screen
+
+wn = turtle.Screen()
+wn.title("Snake game by Kate")
+wn.bgcolor("green")
+wn.setup(width=600, height=600)
+wn.tracer(0) #turns off the screen updates
+
+
+#Snake head
+
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("black")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
+
+#Snake food
+
+food = turtle.Turtle()
+food.speed(0)
+food.shape("circle")
+food.color("red")
+food.penup()
+food.goto(0,100)
+
+
+segments = []
+
+#Funktions
+
+def go_up():
+    head.direction = "up"
+
+
+def go_down():
+    head.direction = "down"
+
+
+def go_left():
+    head.direction = "left"
+
+
+def go_right():
+    head.direction = "right"
+
+
+def move():
+    if head.direction == "up":
+        y = head.ycor() #ycor 0
+        head.sety(y + 4)
+
+    if head.direction == "down":
+        y = head.ycor() #ycor 0
+        head.sety(y - 4)
+
+    if head.direction == "left":
+        x = head.xcor() #ycor 0
+        head.setx(x - 4)
+
+    if head.direction == "right":
+        x = head.xcor() #ycor 0
+        head.setx(x + 4)
+
+#Keyboard bindings - powiazania klawiatury
+
+wn.listen()
+wn.onkeypress(go_up, "w")
+wn.onkeypress(go_down, "s")
+wn.onkeypress(go_left, "a")
+wn.onkeypress(go_right, "d")
+
+#Main game loop
+
+while True:
+    wn.update()
+
+    # Check for a collision with the border
+
+    if head.xcor()>290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
+        time.sleep(0.1)
+        head.goto(0,0)
+        head.direction = "stop"
+
+        # Hide the segments
+
+        for segment in segments:
+            segment.goto(1000, 1000)
+
+        # Clear the segments list
+        segments.clear()
+
+    #Check for a collision with the food
+    if head.distance(food) < 20:
+        # Move the food to a random spot
+        x = random.randint(-290, 290)
+        y = random.randint(-290, 290)
+        food.goto(x,y)
+
+        #Add a segment
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("grey")
+        new_segment.penup()
+        segments.append(new_segment)
+
+    # Move the end segments first in reverse order
+    for index in range(len(segments)-1, 0, -1):
+        x = segments[index -1].xcor()
+        y = segments[index -1].ycor()
+        segments[index].goto(x, y)
+
+    # Mave segment 0 to where the head is
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x,y)
+
+    move()
+
+    time.sleep(delay)
+
+wn.mainloop()
